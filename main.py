583e46da5999cabb47936cc5f93ed967ffb22fb3
patrickbui01 <bui.patrick01@gmail.com>
2025-03-23 22:08:16 +0100

    cleaned code

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from update_pipeline import UpdatePipeline
 
 
 # Assuming you have already created an instance of UpdatePipeline
-update_pipeline = UpdatePipeline("gitlab_parameters.json", "huggingface_parameters.json", "dataset_kadi_apy_doc_params.json", "dataset_kadi_apy_library_params.json", "update_history.json")
+update_pipeline = UpdatePipeline("gitlab_hf_settings.json", "datasets_config.json", "update_history.json")
 
 # Call the method and unpack the returned values
-is_needed, version_tag = update_pipeline.is_update_needed()
-print(is_needed)
-if is_needed:
-    update_pipeline.start_update_pipeline()
+# is_needed, version_tag = update_pipeline.is_update_needed()
+# print(is_needed)
+# if is_needed:
+update_pipeline.start_update_pipeline()
--- update_pipeline.py
+++ update_pipeline.py
@@ -11,22 +11,21 @@ from datetime import datetime, timedelta, timezone
 import time
 import tempfile
 import json
-import shutil
 
+
+   
 class UpdatePipeline:
-    def __init__(self, gitlab_config_path, huggingface_config_path, dataset_kadi_apy_doc_params_path, dataset_kadi_apy_library_params_path, update_history_path):
-        self.gitlab_config_path = gitlab_config_path
-        self.huggingface_config_path = huggingface_config_path
-        self.dataset_kadi_apy_doc_params_path = dataset_kadi_apy_doc_params_path
-        self.dataset_kadi_apy_library_params_path = dataset_kadi_apy_library_params_path
+
+    def __init__(self, gitlab_hf_settings, dataset_params_path, update_history_path):
+        self.gitlab_hf_settings = gitlab_hf_settings
+        self.dataset_params_path = dataset_params_path
         self.update_history_path = update_history_path
-        
+
         # Preloading configurations for efficiency
-        self.gitlab_config = ConfigLoader(self.gitlab_config_path).load()
-        self.huggingface_config = ConfigLoader(self.huggingface_config_path).load()
-        self.kadi_doc_params = ConfigLoader(self.dataset_kadi_apy_doc_params_path).load()
-        self.kadi_library_params = ConfigLoader(self.dataset_kadi_apy_library_params_path).load()
+        self.gitlab_hf_settings = ConfigLoader(self.gitlab_hf_settings).load()
+        self.dataset_params = ConfigLoader(self.dataset_params_path).load()
         self.update_history = ConfigLoader(self.update_history_path).load()
+
 
     def is_update_needed(self):
         latest_release_version_tag = self.get_kadiAPY_latest_release_version_tag()
@@ -60,21 +59,13 @@ class UpdatePipeline:
             kadiAPY_library_documents = self.chunk_kadiAPY_library_files_dataset(kadiAPY_library_files_content, kadiAPY_library_files_path)
             print("4:", len(kadiAPY_library_documents))
 
-
-            temp_dir = tempfile.mkdtemp()
-            self.upload_folder_to_hf(temp_dir)
-            time.sleep(2)  # Allow time for resource release
-            shutil.rmtree(temp_dir, ignore_errors=True)
-
-
-        temp_dir2 = tempfile.mkdtemp()
-        self.embed_documents_into_vectorstore(kadiAPY_doc_documents + kadiAPY_library_documents, get_sfr_embedding_model(), temp_dir2)
-        self.delete_vectorstore_folder_from_huggingface()
-        self.upload_folder_to_hf(temp_dir2)
-        time.sleep(2)
-        shutil.rmtree(temp_dir, ignore_errors=True)
-        self.update_vectorstore_history(version_number)
-
+        # temp_dir2 = tempfile.mkdtemp()
+        # self.embed_documents_into_vectorstore(kadiAPY_doc_documents + kadiAPY_library_documents, get_sfr_embedding_model(), temp_dir2)
+        # self.delete_vectorstore_folder_from_huggingface()
+        # self.upload_folder_to_hf(temp_dir2)
+        # time.sleep(2)
+        # shutil.rmtree(temp_dir, ignore_errors=True)
+        # self.update_vectorstore_history(version_number)
 
     def update_vectorstore_history(self, version_number):
         local_offset_seconds = time.localtime().tm_gmtoff
@@ -84,11 +75,11 @@ class UpdatePipeline:
         new_entry = {
             "update_date": local_time,
             "project_release_version": version_number,
-            "deployed_to_hf_repo": self.huggingface_config["hf_repo_id"]
+            "deployed_to_hf_repo": self.gitlab_hf_settings["huggingface_parameters"]["hf_repo_id"]
         }
 
         try:
-            with open( self.update_history_path, "r") as file:
+            with open(self.update_history_path, "r") as file:
                 history_data = json.load(file)
         except FileNotFoundError:
             history_data = {"update_history": []}
@@ -101,9 +92,9 @@ class UpdatePipeline:
         print(f"Updated vectorstore history with version: {version_number}")
 
     def delete_vectorstore_folder_from_huggingface(self):
-        hf_repo_id = self.huggingface_config["hf_repo_id"]
-        hf_repo_type = self.huggingface_config["hf_repo_type"]
-        hf_vectorstore_path = self.huggingface_config["hf_vectorstore_path"]
+        hf_repo_id = self.gitlab_hf_settings["huggingface_parameters"]["hf_repo_id"]
+        hf_repo_type = self.gitlab_hf_settings["huggingface_parameters"]["hf_repo_type"]
+        hf_vectorstore_path = self.gitlab_hf_settings["huggingface_parameters"]["hf_vectorstore_path"]
 
         if check_folder_exists(hf_repo_id, hf_vectorstore_path):
             delete_folder_from_huggingface(hf_vectorstore_path, hf_repo_id, hf_repo_type)
@@ -111,9 +102,11 @@ class UpdatePipeline:
             print(f"Skipping deletion: Folder '{hf_vectorstore_path}' does not exist in repository '{hf_repo_id}'.")
 
     def upload_folder_to_hf(self, temp_dir):
-        hf_repo_id = self.huggingface_config["hf_repo_id"]
-        hf_repo_type = self.huggingface_config["hf_repo_type"]
-        hf_vectorstore_path = self.huggingface_config["hf_vectorstore_path"]
+        hf_repo_id = self.gitlab_hf_settings["huggingface_parameters"]["hf_repo_id"]
+        hf_repo_type = self.gitlab_hf_settings["huggingface_parameters"]["hf_repo_type"]
+        hf_vectorstore_path = self.gitlab_hf_settings["huggingface_parameters"]["hf_vectorstore_path"]
+
+        upload_folder_to_huggingface(temp_dir, hf_repo_id, hf_repo_type, hf_vectorstore_path)
 
         upload_folder_to_huggingface(temp_dir, hf_repo_id, hf_repo_type, hf_vectorstore_path)
 
@@ -122,25 +115,30 @@ class UpdatePipeline:
         new_vectorstore.add_documents(documents)
 
     def chunk_kadiAPY_doc_dataset(self, doc_files_content, doc_files_path):
-        chunk_size = self.kadi_doc_params["chunking"]["chunking_size"]
-        chunk_overlap = self.kadi_doc_params["chunking"]["chunking_overlap"]
-        dataset_name = self.kadi_doc_params["dataset"]
+        doc_params = self.dataset_params["datasets"]["kadi_apy_docs"]
+        chunk_size = doc_params["chunking"]["chunking_size"]
+        chunk_overlap = doc_params["chunking"]["chunking_overlap"]
+        dataset_name = doc_params["dataset"]
         kadiAPY_doc_documents = chunk_text_and_add_metadata(doc_files_content, doc_files_path, chunk_size, chunk_overlap)
         self.add_dataset_metadata(kadiAPY_doc_documents, dataset_name)
         return kadiAPY_doc_documents
 
     def chunk_kadiAPY_library_files_dataset(self, kadiAPY_library_files_content, kadiAPY_library_files_content_path):
-        dataset_name = self.kadi_library_params["dataset"]
+        library_params = self.dataset_params["datasets"]["kadi_apy_source_code"]
+        dataset_name = library_params["dataset"]
         kadiAPY_library_documents = chunk_pythoncode_and_add_metadata(kadiAPY_library_files_content, kadiAPY_library_files_content_path)
         self.add_dataset_metadata(kadiAPY_library_documents, dataset_name)
         return kadiAPY_library_documents
+    
 
     def get_kadiAPY_doc_dataset(self, repo_zip_filepath):
-        directories_of_kadi_apy_docs = self.kadi_doc_params.get("folder", [])
+        doc_params = self.dataset_params["datasets"]["kadi_apy_docs"]
+        directories_of_kadi_apy_docs = doc_params.get("folder", [])
         return extract_and_process_zip(directories_of_kadi_apy_docs, repo_zip_filepath, filter_filetypes=["rst"])
 
     def get_kadiAPY_library_dataset(self, repo_zip_filepath):
-        directory_of_kadi_apy_library_source_code = self.kadi_library_params.get("folder", [])
+        library_params = self.dataset_params["datasets"]["kadi_apy_source_code"]
+        directory_of_kadi_apy_library_source_code = library_params.get("folder", [])
         return extract_and_process_zip(directory_of_kadi_apy_library_source_code, repo_zip_filepath, filter_filetypes=["py"])
 
     def add_dataset_metadata(self, documents, dataset_name):
@@ -154,14 +152,10 @@ class UpdatePipeline:
         """Read the used gitlab_project_version from the first entry in the JSON file."""
         data = ConfigLoader(self.update_history_path).load()
 
-        first_entry = data["update_history"][0]  # Extract the first entry
-        return first_entry["project_release_version"]  # Correctly return the value
+        first_entry = data["update_history"][0] 
+        return first_entry["project_release_version"]  
 
-    def get_kadiAPY_latest_release_version_tag(self):
-        gitlab_api_url = self.gitlab_config["api_url"]
-        gitlab_project_id = self.gitlab_config["project id"]
-   
-        return get_latest_release_version_tag(gitlab_api_url, gitlab_project_id)
+
 
     def is_newer_version_available(self, version1, version2):
         """Compare two versions and check if version1 is newer than version2."""
@@ -173,10 +167,15 @@ class UpdatePipeline:
         else:
             return False  # Returns False if versions are equal or version1 is older
 
+    def get_kadiAPY_latest_release_version_tag(self):
+        gitlab_api_url = self.gitlab_hf_settings["gitlab_parameters"]["api_url"]
+        gitlab_project_id = self.gitlab_hf_settings["gitlab_parameters"]["project_id"]
+   
+        return get_latest_release_version_tag(gitlab_api_url, gitlab_project_id)
 
     def download_latest_kadiAPY_repo_for_processing(self, target_path):
-        gitlab_api_url = self.gitlab_config["api_url"]
-        gitlab_project_id = self.gitlab_config["project id"]
+        gitlab_api_url = self.gitlab_hf_settings["gitlab_parameters"]["api_url"]
+        gitlab_project_id = self.gitlab_hf_settings["gitlab_parameters"]["project_id"]
         latest_release_version_tag = self.get_kadiAPY_latest_release_version_tag()
         target_path_zip_file = download_gitlab_repo(gitlab_api_url, gitlab_project_id, latest_release_version_tag, target_path)
 
@@ -184,6 +183,5 @@ class UpdatePipeline:
             "target_path_zip_file": target_path_zip_file,
             "version_number": latest_release_version_tag
         }
-        
-
+    
     
